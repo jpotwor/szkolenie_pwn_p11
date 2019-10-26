4f32522da218444c1d84dc7ca9b975922c72c83a
Janek <jpotworowski@gmail.com>
2019-10-26 16:24:17 +0200

    added random forest

--- analyze_adults.py
+++ analyze_adults.py
@@ -2,6 +2,8 @@ import pandas as pd
 from sklearn.svm import SVC, LinearSVC
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import classification_report
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 houses = pd.read_csv("Adult_train.tab", sep='\t', skiprows=[1, 2])
 
@@ -17,10 +19,7 @@ x = pd.get_dummies(x, columns=one_hot_vars)
 from sklearn.preprocessing import StandardScaler
 
 scaler = StandardScaler()
-
 x = pd.DataFrame(scaler.fit_transform(x))
-
-scale_cols = ['age', 'fnlwgt', 'education-num', 'capital-loss', 'hours-per-week']
 
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
@@ -28,13 +27,34 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 print(len(x_train))
 print(len(x_train.columns))
 
-svc_params = {
-    'C': [0.01, 0.05, 0.1],
+# # svm
+# svc_params = {
+#     'C': [1, 10],
+# }
+#
+# clf = GridSearchCV(SVC(max_iter=10000), svc_params, cv=5, verbose=3, n_jobs=-1)
+
+# decision trees
+# tree_params = {
+#     'criterion': ['gini', 'entropy'],
+#     'max_depth': [5, 6, 8, 10, 20],
+#     'splitter': ['best', 'random']
+# }
+# clf = GridSearchCV(DecisionTreeClassifier(), tree_params, cv=5)
+
+random_forest_params = {
+    'n_estimators': [100],
+    'max_depth': [21],
 }
-clf = GridSearchCV(LinearSVC(max_iter=10000), svc_params, cv=5, verbose=3, n_jobs=-1)
+clf = GridSearchCV(RandomForestClassifier(), random_forest_params, cv=5)
+
 
 clf.fit(x_train, y_train)
 print(clf.best_params_)
 
 y_train_pred = clf.predict(x_train)
 print(classification_report(y_train, y_train_pred))
+
+y_test_pred = clf.predict(x_test)
+print(classification_report(y_test, y_test_pred))
+
