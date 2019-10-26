f4cb5f93defe22992620b429aec9b8992d197e00
Janek <jpotworowski@gmail.com>
2019-10-26 12:26:36 +0200

    added test train split

--- analyze_adults.py
+++ analyze_adults.py
@@ -1,41 +1,12 @@
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
 
 houses = pd.read_csv("Adult_train.tab", sep='\t', skiprows=[1, 2])
 
-y_train = (houses['y'] == '>50K').astype('int')
+y = (houses['y'] == '>50K').astype('int')
 
-x_train = houses.drop(['y'], axis=1)
-x_train = x_train.drop(['education'], axis=1)
+x = houses.drop(['y'], axis=1)
+x = x.drop(['education'], axis=1)
 
-
-def get_one_hot_repr(data, colname):
-    """
-    get one hot representation for a column in a pandas dataframe
-    :param data: data frame
-    :param colname: column name
-    :return:
-    """
-    ohe = OneHotEncoder()
-    result = ohe.fit_transform(data[colname].values.reshape(-1, 1)).toarray()
-    result = pd.DataFrame(result, columns=[colname + str(ohe.categories_[0][i]) for i in range(len(ohe.categories_[0]))])
-    return result
-
-
-def substitute_with_one_hot(data, colnames):
-    """
-    substitute columns from a list with their one hot representations
-    :param data: data frame
-    :param colnames: list of column names
-    :return:
-    """
-    for colname in colnames:
-        one_hot_column = get_one_hot_repr(data, colname)
-        one_hot_column.fillna(0)
-        data = pd.concat([data, one_hot_column], axis=1)
-        del data[colname]
-    return data
-
-
-
-print(x_train['education'].head())
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
